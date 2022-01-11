0e8a0727f71e5c440ac403aee7105499dfa4c07a
Nhien (Ricky) Lam <62775270+NhienLam@users.noreply.github.com>
2022-01-10 22:19:54 -0800

    Create NumberofIslands.java

---
+++ LeetCode/Medium/NumberofIslands.java
@@ -0,0 +1,83 @@
+/***   URL: https://leetcode.com/problems/number-of-islands/   ***/
+
+/*
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+*/
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        
+        HashSet<String> visited = new HashSet<>();
+        
+        for(int r = 0; r < grid.length; r++) {
+            for(int c = 0; c < grid[0].length; c++) {
+                if(explore(grid, r, c, visited)==true){
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+        
+    }
+    
+    public boolean explore(char[][] grid, int r, int c, HashSet<String> visited) {
+        
+        // r or c is out of bound
+        if(!(r >= 0 && r < grid.length)) {
+            return false;    
+        }
+        if(!(c >= 0 && c < grid[0].length)) {
+            return false;    
+        }
+        
+        // not land
+        if(grid[r][c] == '0') {
+            return false;
+        }
+        
+        String index = String.valueOf(r) + "," + String.valueOf(c);
+        if(visited.contains(index)) {
+            return false;
+        }
+        
+        visited.add(index);
+        
+        // explore 4 directions
+        explore(grid, r-1, c, visited);
+        explore(grid, r+1, c, visited);
+        explore(grid, r, c-1, visited);
+        explore(grid, r, c+1, visited);
+        
+        // if everything is true
+        return true; 
+        
+    }
+}
