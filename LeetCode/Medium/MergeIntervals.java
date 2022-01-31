f43897c404232cecb0a102c455e28a0c57962aea
Nhien (Ricky) Lam <62775270+NhienLam@users.noreply.github.com>
2022-01-31 11:31:50 -0800

    Runtime: 10 ms, faster than 48.56% of Java

--- LeetCode/Medium/MergeIntervals.java
+++ LeetCode/Medium/MergeIntervals.java
@@ -1,4 +1,4 @@
-/***   URL: https://leetcode.com/problems/add-two-numbers/   ***/
+/***   URL: https://leetcode.com/problems/merge-intervals/   ***/
 
 /*
 Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
